55860d9b1f152c93004bc1d34ba1ed785e66e80c
Kazuki Tobita <kazukiti201@gmail.com>
2018-02-20 22:14:20 +0900

    地域が表示されるよう変更

--- src/pages/Top.vue
+++ src/pages/Top.vue
@@ -2,24 +2,31 @@
   <div class="pages">
     <h1>Trouble Traveler</h1>
     <button type="button" @click="gacha()">ガチャる</button>
-    <div>{{ travel }}</div>
+    <div>
+      <div>{{ travel.preference }}</div>
+      <div>{{ travel.region }}</div>
+    </div>
   </div>
 </template>
 
 <script>
-  import data from './data.json'
+  import preferences from './preferences.json'
 
   export default {
     data() {
       return {
-        data: data,
-        travel: ''
+        preferences: preferences,
+        travel: {
+          preference: '',
+          region: ''
+        }
       }
     },
     methods: {
       gacha() {
         const id = Math.floor(Math.random() * 46 + 1)
-        this.travel = this.data.result[id].preference
+        this.travel.preference = this.preferences.result[id].preference
+        this.travel.region = this.preferences.result[id].region
       }
     }
   }
